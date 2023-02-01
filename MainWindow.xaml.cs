0b398d1f099dd285c89c1608088630e094294e04
Denys Samoilenko <denniksam@gmail.com>
2023-02-01 11:06:02 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Data.SqlClient;   // Не забути NuGet
+
+namespace ADO_201
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // об'єкт підключення: головний елемент ADO
+        private SqlConnection _connection;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            // !! Створення об'єкта не відкриває підключення
+            _connection = new();
+            // Головний параметр підключення - рядок підключення
+            _connection.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\_dns_\source\repos\ADO-201\ADO-201.mdf;Integrated Security=True";
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                _connection.Open();
+                StatusConnection.Content = "Connected";
+                StatusConnection.Foreground = Brushes.Green;
+            }
+            catch(SqlException ex)
+            {
+                StatusConnection.Content = "Disconnected";
+                StatusConnection.Foreground = Brushes.Red;
+                MessageBox.Show(ex.Message);
+                this.Close();
+            }
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            if (_connection?.State == System.Data.ConnectionState.Open)
+            {
+                _connection.Close();
+            }
+        }
+    }
+}
